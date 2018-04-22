f95c27ed94286a1d4d66fdce4569188e7cb9ad38
blinded93 <blinded93@gmail.com>
2018-04-21 23:14:23 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', function listener(event) {
+    const key = parseInt(event.detail);
+    if (key === code[index]) {
+      ++index;
+    } else {
+      index = 0;
+    };
+    if (index === code.length) {
+      alert("You guessed it! Great job!")
+      index = 0
+    };
+  })
 }
